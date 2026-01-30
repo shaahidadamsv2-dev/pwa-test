5e1d1357d3269610e799aa7b04efec58bc0119f9
shaahidadamsv2-dev <shaahid.adamsv2@gmail.com>
2026-01-30 20:49:43 +0200

    testing some changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ const Index = () => (
 	<Page>
 		<Section>
 			<h2 className='text-xl font-semibold text-zinc-800 dark:text-zinc-200'>
-				We grow a lot of rice.
+				This is a change I've made!
 			</h2>
 
 			<div className='mt-2'>
